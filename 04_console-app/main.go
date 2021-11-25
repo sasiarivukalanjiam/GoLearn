ccff7bbc0170d2b6609c6536b6950694087e78a2
sasiarivukalanjiam <sasiarivukalanjiam@gmail.com>
2021-11-25 18:35:59 +0530

    created folder for console app
    folder structure created

---
+++ 04_console-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(userInput)
+		}
+	}
+}
